7e2b8beab48c328f2b26b71dd967cbe3d40f12d1
Fredrik Johansson <fredrik@everyloop.com>
2024-08-29 10:19:57 +0200

    Add project L004_Felsökning

---
+++ Code-alongs/L004_Felsökning/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine('A');
+Console.WriteLine('B');
+Console.WriteLine('C');
+
+int z = Int32.Parse(Console.ReadLine());
+
+int x = 0;
+int y = 0;
+while (x < 10)
+{
+    Console.WriteLine($"x = {x}, y = {y}");
+    x++;
+
+    if (x % 2 == 0)
+    {
+        y++;
+    }
+}
+
