f52a3c183211b23a8e348b67b79a4d35e3b4e12b
Fredrik Johansson <fredrik@everyloop.com>
2024-09-12 12:10:12 +0200

    Add project L016_FileStream

---
+++ Code-alongs/L016_FileStream/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using (FileStream stream = File.OpenRead("L016_FileStream.exe"))
+{
+    Console.WriteLine(stream.Length);
+
+    byte[] data = new byte[stream.Length];
+    stream.Read(data);
+
+    for (int i = 0; i < 100; i++)
+    {
+        Console.Write($"{data[i].ToString("X2")} ");
+    }
+}
+
+
