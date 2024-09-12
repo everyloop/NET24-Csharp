a1a9de1e6d3581d13c2fb0cf9f599a1ee288d11d
Fredrik Johansson <fredrik@everyloop.com>
2024-09-12 11:16:51 +0200

    Update project L015, cleaning up code

--- Code-alongs/L015_StreamWriter_och_StreamReader/Program.cs
+++ Code-alongs/L015_StreamWriter_och_StreamReader/Program.cs
@@ -1,25 +1,29 @@
-﻿
+﻿Console.WriteLine("*** StreamWriter *******************");
 
-//Console.WriteLine(Directory.GetCurrentDirectory());
+Console.WriteLine(Directory.GetCurrentDirectory());
 
-//using (StreamWriter writer = new StreamWriter("myfile.txt"))
-//{
-//    for (int i = 0; i < 9; i++)
-//    {
-//        //Console.WriteLine($"Hello {i}!");
-//        writer.WriteLine($"Hello {i}!");
-//    }
-//}
+using (StreamWriter writer = new StreamWriter("myfile.txt"))
+{
+    for (int i = 0; i < 9; i++)
+    {
+        Console.WriteLine($"Hello {i}!");
+        writer.WriteLine($"Hello {i}!");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("*** StreamReader *******************");
 
 using (StreamReader reader = new StreamReader("myfile.txt"))
 {
-    //while (!reader.EndOfStream)
-    //{
-    //    //Console.Write((char)reader.Read());
+    while (!reader.EndOfStream)
+    {
+        //Thread.Sleep(50);
+        //Console.Write((char)reader.Read());
 
-    //    Thread.Sleep(200);
-    //    Console.WriteLine(reader.ReadLine());
-    //}
+        //Thread.Sleep(200);
+        Console.WriteLine(reader.ReadLine());
+    }
 
-    Console.Write(reader.ReadToEnd());
+    //Console.Write(reader.ReadToEnd());
 }
