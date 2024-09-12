75b6cfe4c0f6268d3af6a200e62dd32a06d1763b
Fredrik Johansson <fredrik@everyloop.com>
2024-09-12 11:10:33 +0200

    Add project L015_StreamWriter_och_StreamReader

---
+++ Code-alongs/L015_StreamWriter_och_StreamReader/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//Console.WriteLine(Directory.GetCurrentDirectory());
+
+//using (StreamWriter writer = new StreamWriter("myfile.txt"))
+//{
+//    for (int i = 0; i < 9; i++)
+//    {
+//        //Console.WriteLine($"Hello {i}!");
+//        writer.WriteLine($"Hello {i}!");
+//    }
+//}
+
+using (StreamReader reader = new StreamReader("myfile.txt"))
+{
+    //while (!reader.EndOfStream)
+    //{
+    //    //Console.Write((char)reader.Read());
+
+    //    Thread.Sleep(200);
+    //    Console.WriteLine(reader.ReadLine());
+    //}
+
+    Console.Write(reader.ReadToEnd());
+}
