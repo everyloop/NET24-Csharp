e84fb29eb518461479d55b26dd9c2ca93f084f6d
Fredrik Johansson <fredrik@everyloop.com>
2024-09-18 14:11:44 +0200

    Add project L018

---
+++ Code-alongs/L018_Static_classes_and_members/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Cat myCat = new Cat() { name = "Pelle" };
+
+// Cat.numberOfLives = 7;
+
+myCat.Greet();
+
+Cat.PrintNumberOfLives();
+
+// Om klassen i sig är static kan man inte instantiera objekt alls.
+// Ex: static class Cat
+
+class Cat
+{
+    public string name;
+    
+    public static int numberOfLives = 9;
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hi, my name is {name}, and I have {numberOfLives} lives.");
+    }
+
+    public static void PrintNumberOfLives()
+    {
+        Console.WriteLine($"Every cat has {numberOfLives} lives.");
+    }
+
+}
