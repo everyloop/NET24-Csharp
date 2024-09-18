76821266f8b88483421deaad71e1f051422adaf4
Fredrik Johansson <fredrik@everyloop.com>
2024-09-18 14:13:29 +0200

    Add project L020

---
+++ Code-alongs/L020_Namespaces_and_Using/Program.cs
@@ -0,0 +1,32 @@
+﻿using myNamespace.anotherNamespace;
+using myNamespace;
+using System;
+
+Cat.SayHello();
+
+Dog.SayHello();
+
+Console.WriteLine();
+
+namespace myNamespace
+{
+    class Cat
+    {
+        static public void SayHello()
+        {
+            Console.WriteLine("Hello! I'm a cat");
+        }
+    }
+
+}
+
+namespace myNamespace.anotherNamespace
+{
+    class Dog
+    {
+        static public void SayHello()
+        {
+            Console.WriteLine("Hello! I'm a dog");
+        }
+    }
+}
