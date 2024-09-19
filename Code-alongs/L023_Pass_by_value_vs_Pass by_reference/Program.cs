1184f9d3f0ca67a8d34d300c3fdca64930bd7f25
Fredrik Johansson <fredrik@everyloop.com>
2024-09-19 12:08:40 +0200

    Add project L023 and L024

---
+++ Code-alongs/L023_Pass_by_value_vs_Pass by_reference/Program.cs
@@ -0,0 +1,82 @@
+﻿
+// Value type
+int a = 5;
+int b = a;
+a = 3;
+
+Console.WriteLine($"a = {a}, b = {b}");
+
+// Reference type
+Cat catA = new() { name = "Pelle" };
+Cat catB = catA;
+
+catA.name = "Måns";
+
+Console.WriteLine($"catA.name = {catA.name}, catB.name = {catB.name}");
+
+Console.WriteLine();
+
+
+
+// Exempel på hur value types kan skickas till en metod som pass by value eller pass by reference.
+
+PassValueTypeByValue(a);
+Console.WriteLine($"After PassValueTypeByValue(a) => a = {a}");
+
+PassValueTypeByReference(ref a);
+Console.WriteLine($"After PassValueTypeByReference(a) => a = {a}");
+
+static void PassValueTypeByValue(int i)
+{
+    i = 0;
+    //Console.WriteLine(i);
+}
+
+static void PassValueTypeByReference(ref int i)
+{
+    i = 0;
+    //Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+
+
+// Exempel på hur reference types kan skickas till en metod som pass by value eller pass by reference.
+
+PassReferenceTypeByValue(catA);
+Console.WriteLine($"After PassReferenceTypeByValue(catA) => catA.name = {catA.name}");
+
+PassReferenceTypeByReference(ref catA);
+Console.WriteLine($"After PassReferenceTypeByReference(catA) => catA.name = {catA.name}");
+
+static void PassReferenceTypeByValue(Cat cat)
+{
+    // Uppdaterar man referensen cat, så påverkar det inte variabeln i anropet (catA).
+    cat = new Cat() { name = "Bosse" };
+
+    // Däremot kan fields och properties på objektet uppdateras även i en pass by value.
+    //cat.name = "Bosse";
+
+    //Console.WriteLine(cat.name);
+}
+
+static void PassReferenceTypeByReference(ref Cat cat)
+{
+    // Uppdaterar man referensen cat, så uppdateras även referensen i argumentet i anropet (catA).
+    cat = new Cat() { name = "Bosse" };
+
+    // Fields och properties på objektet kan alltid uppdateras.
+    //cat.name = "Bosse";
+
+    //Console.WriteLine(cat.name);
+}
+
+
+
+class Cat()
+{
+    public string name;
+}
+
+
---
+++ Code-alongs/L024_Struct_och_Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Position playerPosition = new Position(0, 0);
+
+Console.WriteLine($"Player is at position ({playerPosition.X}, {playerPosition.Y})");
+
+Color myColor = Color.Green;
+
+Console.WriteLine($"myColor = {myColor} ({(int)myColor})");
+Console.WriteLine((Color)3);
+
+// En struct fungerar i princip som en klass men definerar en value type istället för en reference type.
+struct Position
+{
+    public int X;
+    public int Y;
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+// Enum (enumeration types) är också value types. De används om man vill ha variabler som kan anta
+// ett av de värden som man definerat upp i sin enum.
+enum Color { Red, Green, Blue, Yellow, Purple };
