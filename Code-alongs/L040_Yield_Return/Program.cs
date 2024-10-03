66b5699b6ba6e905850956d7015dfdc4857284d8
Fredrik Johansson <fredrik@everyloop.com>
2024-10-03 12:10:25 +0200

    Add project L040

---
+++ Code-alongs/L040_Yield_Return/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Collections;
+
+foreach(var number in GetNumbers())
+{
+    Console.WriteLine(number);
+}
+
+IEnumerable GetNumbers()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		yield return i;
+	}
+}
