b7c70b0719e9e92a6a254be36ba241461cd6a1df
Fredrik Johansson <fredrik@everyloop.com>
2024-10-08 21:30:02 +0200

    Add L041, L042

---
+++ Code-alongs/L041_PLINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+var list = Enumerable
+    .Range(0, 50)
+    .AsParallel()
+    .Select(i => CostlyOperation(i))
+    .ToList();
+
+Console.WriteLine("\nProcessing complete! Here is the processed data:\n");
+list.ForEach(i => Console.WriteLine(i));
+
+static int CostlyOperation(int n)
+{
+    for (int i = 0; i < 1000_000_000; i++) ;
+
+    Console.WriteLine($"Processing element {n}.");
+    return n;
+}
---
+++ Code-alongs/L042_Tasks/Program.cs
@@ -0,0 +1,55 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Task<long> task1 = new Task<long>(() =>
+        {
+            long sum = 0;
+
+            for (int i = 1; i <= 500; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        });
+
+        Console.WriteLine($"task1 is {task1.Status}");
+
+        Task myContinuationTask = task1.ContinueWith(task =>
+        {
+            Console.WriteLine($"Result of task1: {task.Result}");
+        });
+
+        task1.Start();
+
+        Console.WriteLine($"task1 is {task1.Status}");
+
+        //Task1();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine($"Main thread: {i}");
+        }
+
+        Console.WriteLine("Main thread continues ...");
+
+        //Task.WaitAll(task1);
+
+
+        Console.WriteLine($"task1 is {task1.Status}");
+
+        Console.WriteLine("The end!");
+
+        Console.ReadKey(true);
+
+        //static void Task1()
+        //{
+        //    for (int i = 0; i < 2000; i++)
+        //    {
+        //        Console.WriteLine($"Task1: {i}");
+        //    }
+        //}
+    }
+}
