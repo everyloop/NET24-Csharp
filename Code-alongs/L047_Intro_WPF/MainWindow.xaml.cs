21e6d5347a72d0250fcca08514aebe417dc0cdbf
Fredrik Johansson <fredrik@everyloop.com>
2024-10-14 11:32:55 +0200

    Add project L047

---
+++ Code-alongs/L047_Intro_WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L047_Intro_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Button button = new Button() { Content = "Hello" };
+            //button.Content = "Hello";
+            //button.HorizontalAlignment = HorizontalAlignment.Left;
+
+            var stackPanel = new StackPanel();
+
+            stackPanel.Children.Add(new Button());
+            stackPanel.Children.Add(new Label());
+
+        }
+    }
+}
